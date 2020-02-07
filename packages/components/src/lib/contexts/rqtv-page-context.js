5a6af84e75ee30d988b82a6241fb4c6a5aac5d5c
paolo <paoloandreaderegibus@yahoo.it>
2020-02-07 18:10:17 +0100

    added condition handling to rqtvPage

--- packages/components/src/lib/contexts/rqtv-page-context.js
+++ packages/components/src/lib/contexts/rqtv-page-context.js
@@ -2,17 +2,46 @@
 //Copyright (c) 2019 by Paolo Deregibus. All Rights Reserved.
 //
 
-import React from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import PropTypes from 'prop-types';
-import {useTriggers} from '@reaqtive/q'
+import {useQObjectReducer, useQLayoutReducer, useTriggers} from '@reaqtive/q'
 
 const RqtvPageContext = React.createContext()
 
+const useQPageObjectDef = (qConditionExpr) => useMemo(()=>{
+  return {
+    qInfo: {
+      qType: "tableData"
+    },
+    qCondition: {
+      qStringExpression: {
+        qExpr: qConditionExpr
+      }
+    }
+  }
+}, [qConditionExpr])
+
 const RqtvPageConsumer = props => {
   const triggerState = useTriggers(props.triggers)
+  const qConditionExpr = props.conditionExpr;
+  const qObjectDef=useQPageObjectDef(qConditionExpr)
+  const qObjectHandler = useQObjectReducer(qObjectDef)
+  const qLayoutHandler = useQLayoutReducer(qObjectHandler)
+  const qCondition = qLayoutHandler.qLayout&&qLayoutHandler.qLayout.qCondition
+  const [conditionRes, setconditionRes] = useState()
+
+  useEffect(()=>{
+    if(qCondition==='0'){
+      setconditionRes(false)
+    }
+    if(qCondition==='-1'){
+      setconditionRes(true)
+    }
+  },[qCondition])
+
   return(
     <RqtvPageContext.Provider
-      value={{triggerState, pageData:props.pageData}}
+      value={{triggerState, pageData:props.pageData, conditionRes}}
     >
       {props.children}
     </RqtvPageContext.Provider>
@@ -28,11 +57,13 @@ const RqtvPageProvider = props => {
 }
 
 RqtvPageProvider.propTypes = {
-  triggers:PropTypes.array.isRequired
+  triggers:PropTypes.array.isRequired,
+  conditionExpr:PropTypes.string
 }
 
 RqtvPageProvider.defaultProps = {
-  triggers:[]
+  triggers:[],
+  conditionExpr:""
 }
 
 export {RqtvPageProvider, RqtvPageContext}
