a158186c61043ae4994c0b220e5deffbb3a46480
paolo <paoloandreaderegibus@yahoo.it>
2020-02-11 18:05:26 +0100

    fixed query string triggers behaviour

--- packages/components/src/lib/pages/rqtv-page.js
+++ packages/components/src/lib/pages/rqtv-page.js
@@ -2,18 +2,31 @@
 //Copyright (c) 2019 by Paolo Deregibus. All Rights Reserved.
 //
 
-import React, {useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import PropTypes from 'prop-types'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, useLocation } from 'react-router-dom'
 import {RqtvPageContext, RqtvPageProvider} from '../contexts/rqtv-page-context'
 
 const RqtvPage = props => {
   const pageData={path:props.path, title:props.title, id:props.id}
   const {fallbackPage} = props
+  const location = useLocation()
+  const [hasChangedLocation, setHasChangedLocation] = useState(false)
+  useEffect(()=>{
+    const delay = location.search!==""?500:0
+    setTimeout(()=>setHasChangedLocation(true),delay)
+    //console.log(delay)
+    return () => setHasChangedLocation(false)
+  },[location])
   return(
     <Route path={props.path} exact={props.exact}>
-      <RqtvPageProvider triggers={props.triggers} pageData={pageData} conditionExpr={props.conditionExpr}>
-        <RqtvPageConsumer fallbackPage={fallbackPage}>
+      <RqtvPageProvider
+        triggers={props.triggers}
+        pageData={pageData}
+        conditionExpr={props.conditionExpr}
+        hasQueryString={location.search!==""?true:false}
+      >
+        <RqtvPageConsumer fallbackPage={fallbackPage} hasChangedLocation={hasChangedLocation}>
           {props.children}
         </RqtvPageConsumer>
       </RqtvPageProvider>
@@ -26,7 +39,7 @@ const RqtvPageConsumer = props => {
   const {conditionRes, triggerState}=rqtvPageContext&&rqtvPageContext
   const {fallbackPage}=props
 
-  if(conditionRes===false && fallbackPage!=="" && triggerState.done===true){
+  if(props.hasChangedLocation===true && conditionRes===false && fallbackPage!=="" && triggerState.done===true){
     return <Redirect to={fallbackPage} />
   }
   return  props.children
