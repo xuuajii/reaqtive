7b5e0cc346827ec58ea9909959f09cc856edea44
paolo <paoloandreaderegibus@yahoo.it>
2020-02-11 18:05:51 +0100

    transpiled previous commits

--- packages/components/dist/contexts/rqtv-page-context.js
+++ packages/components/dist/contexts/rqtv-page-context.js
@@ -46,23 +46,42 @@ const useQPageObjectDef = (qConditionExpr, qTitleExpr) => (0, _react.useMemo)(()
 }, [qConditionExpr]);
 
 const RqtvPageConsumer = props => {
-  const location = (0, _reactRouterDom.useLocation)(); //console.log(currentLocation)
+  const location = (0, _reactRouterDom.useLocation)(); // const [locationSearch, setLocationSearch] = useState(null)
+  // useEffect(()=>{
+  //   setLocationSearch(location.search)
+  //   return () => setLocationSearch(null)
+  // },[location.search])
 
   const queryStringTriggers = (0, _index.useQueryString)(location.search);
-  const triggers = queryStringTriggers ? [...props.triggers, ...queryStringTriggers] : [...props.triggers];
-  const triggersDone = (0, _q.useTriggers)([...triggers]);
-  const initialTriggerState = (0, _react.useRef)(triggersDone);
-  const triggerState = Array.isArray(queryStringTriggers) ? triggersDone : initialTriggerState.current;
+
+  const _useState = (0, _react.useState)(null),
+        _useState2 = (0, _slicedToArray2.default)(_useState, 2),
+        triggers = _useState2[0],
+        setTriggers = _useState2[1];
+
+  (0, _react.useEffect)(() => {
+    if (location.search !== null && location.search !== '' && Array.isArray(queryStringTriggers) && queryStringTriggers.length > 0) {
+      setTriggers([...props.triggers, ...queryStringTriggers]);
+    }
+
+    if (location.search === '') {
+      setTriggers([...props.triggers]);
+    }
+
+    return () => setTriggers(null);
+  }, [props.triggers, queryStringTriggers, location.search]);
+  const triggerState = (0, _q.useTriggers)(triggers); //console.log(location.search,triggers, queryStringTriggers)
+
   const qConditionExpr = props.conditionExpr;
   const qObjectDef = useQPageObjectDef(qConditionExpr);
   const qObjectHandler = (0, _q.useQObjectReducer)(qObjectDef);
   const qLayoutHandler = (0, _q.useQLayoutReducer)(qObjectHandler);
   const qCondition = qLayoutHandler.qLayout && qLayoutHandler.qLayout.qCondition;
 
-  const _useState = (0, _react.useState)(),
-        _useState2 = (0, _slicedToArray2.default)(_useState, 2),
-        conditionRes = _useState2[0],
-        setConditionRes = _useState2[1];
+  const _useState3 = (0, _react.useState)(),
+        _useState4 = (0, _slicedToArray2.default)(_useState3, 2),
+        conditionRes = _useState4[0],
+        setConditionRes = _useState4[1];
 
   const qTitle = qLayoutHandler.qLayout && qLayoutHandler.qLayout.qTitle;
   (0, _react.useEffect)(() => {
@@ -73,6 +92,8 @@ const RqtvPageConsumer = props => {
     if (qCondition === '-1') {
       setConditionRes(true);
     }
+
+    return () => setConditionRes(null);
   }, [qCondition]);
   return _react.default.createElement(RqtvPageContext.Provider, {
     value: {
@@ -83,7 +104,7 @@ const RqtvPageConsumer = props => {
     },
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 59
+      lineNumber: 75
     },
     __self: void 0
   }, props.children);
@@ -93,7 +114,7 @@ const RqtvPageProvider = props => {
   return _react.default.createElement(RqtvPageConsumer, Object.assign({}, props, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 69
+      lineNumber: 87
     },
     __self: void 0
   }), props.children);
