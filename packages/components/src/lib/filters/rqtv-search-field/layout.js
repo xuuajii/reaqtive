ade13d273b117b4e517bd7bce0cc6ab1253d8c68
paolo deregibus <paolo.deregibus@gmail.com>
2020-03-10 11:19:37 +0100

    fixed data availability check in filters

--- packages/components/src/lib/filters/rqtv-search-field/layout.js
+++ packages/components/src/lib/filters/rqtv-search-field/layout.js
@@ -13,7 +13,7 @@ const Layout = props => {
     const qLayout = props.qLayoutHandler&&props.qLayoutHandler.qLayout
     const qDataPages = qLayout&&qLayout.qListObject.qDataPages
     const qSize = qLayout&&qLayout.qListObject.qSize
-    const qArea = qLayout&&qLayout.qListObject.qDataPages[0].qArea
+    const qArea = qDataPages&&qDataPages.length>0&&qDataPages[0].qArea
     const rendererProps = useListObjectRendererMap(props.qLayoutHandler, props.qObjectHandler)
 
     const {rqtvListObject} = props
