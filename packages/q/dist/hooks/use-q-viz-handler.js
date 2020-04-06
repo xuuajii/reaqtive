6eb9ea78696623923600957b71b9450e12e8a96b
paolo <paoloandreaderegibus@yahoo.it>
2020-04-06 09:38:43 +0200

    added destroy object to qObjectReducer and reviewed qVizHandler to limit rerenders and improve performances

--- packages/q/dist/hooks/use-q-viz-handler.js
+++ packages/q/dist/hooks/use-q-viz-handler.js
@@ -9,6 +9,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _objectSpread2 = _interopRequireDefault(require("@babel/runtime/helpers/esm/objectSpread"));
+
 var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/esm/slicedToArray"));
 
 var _react = _interopRequireWildcard(require("react"));
@@ -48,26 +50,44 @@ const useQVizHandler = (qApp, id, chartProps) => {
         qVizHandler = _useState2[0],
         setQVizHandler = _useState2[1];
 
-  if (chartProps && qVizHandler.qVizLoading === true) {
-    qApp && qApp.visualization.create(chartProps.chartType, chartProps.chartColumns, chartProps.rest).then(qViz => setQVizHandler({
-      qVizLoading: false,
-      qViz: qViz
-    }));
-  }
-
-  if (id && qVizHandler.qVizLoading === true && !chartProps) {
-    qApp && qApp.visualization.get(id).then(qViz => setQVizHandler({
-      qVizLoading: false,
-      qViz: qViz
-    }));
-  }
+  const _useState3 = (0, _react.useState)(null),
+        _useState4 = (0, _slicedToArray2.default)(_useState3, 2),
+        vizId = _useState4[0],
+        setVizId = _useState4[1];
 
   (0, _react.useEffect)(() => {
-    if (qVizHandler.qViz) {
-      qVizHandler.qViz.show(id);
+    if (chartProps && vizId === null) {
+      //qVizHandler.qVizLoading===true && qVizHandler.qViz===null && qApp!==null && qApp !== undefined){
+      qApp && qApp.visualization.create(chartProps.chartType, chartProps.chartColumns, chartProps.rest).then(qViz => {
+        setVizId(qViz.id);
+      });
     }
-  }, [qVizHandler, id]);
-  return qVizHandler;
+
+    if (id && !chartProps) {
+      setVizId(id);
+    }
+
+    if (vizId !== null && qVizHandler.qVizLoading === true) {
+      qApp && qApp.visualization.get(vizId).then(qViz => setQVizHandler({
+        qVizLoading: false,
+        qViz: qViz
+      }));
+    }
+  }, [qApp, chartProps, vizId, id, qVizHandler.qVizLoading]);
+  (0, _react.useEffect)(() => {
+    if (qVizHandler.qViz) {
+      qVizHandler.qViz.show(vizId);
+    }
+
+    return () => {
+      if (qVizHandler.qViz !== null) {
+        qApp && qApp.destroySessionObject(vizId);
+      }
+    };
+  }, [qVizHandler, vizId]);
+  return (0, _objectSpread2.default)({}, qVizHandler, {
+    vizId
+  });
 };
 
 var _default = useQVizHandler;
--- packages/q/src/lib/hooks/use-q-viz-handler.js
+++ packages/q/src/lib/hooks/use-q-viz-handler.js
@@ -28,27 +28,40 @@ import React from 'react'
 const useQVizHandler = (qApp, id, chartProps) => {
   const qVizInitialState = {qVizLoading:true, qViz:null}
   const [qVizHandler, setQVizHandler] = useState(qVizInitialState)
-
-  if(chartProps && qVizHandler.qVizLoading===true){
-    qApp&&qApp.visualization.create(
-      chartProps.chartType,
-      chartProps.chartColumns,
-      chartProps.rest
-    )
-    .then(qViz => setQVizHandler({qVizLoading:false, qViz:qViz}))
-  }
-
-  if(id && qVizHandler.qVizLoading===true && !(chartProps)){
-    qApp&&qApp.visualization.get(id)
-    .then(qViz => setQVizHandler({qVizLoading:false, qViz:qViz}))
-  }
+  const [vizId, setVizId] = useState(null)
 
   useEffect(()=>{
-    if(qVizHandler.qViz){
-      qVizHandler.qViz.show(id)
+    if(chartProps && vizId===null ){//qVizHandler.qVizLoading===true && qVizHandler.qViz===null && qApp!==null && qApp !== undefined){
+      qApp&&qApp.visualization.create(
+        chartProps.chartType,
+        chartProps.chartColumns,
+        chartProps.rest
+      )
+      .then(qViz => {
+        setVizId(qViz.id)
+      })
     }
-  }, [qVizHandler, id])
-  return(qVizHandler)
-}
+    if(id && !(chartProps)){
+      setVizId(id)
+    }
+
+    if(vizId!==null && qVizHandler.qVizLoading===true){
+      qApp&&qApp.visualization.get(vizId)
+      .then(qViz => setQVizHandler({qVizLoading:false, qViz:qViz}))
+    }
+  },[qApp, chartProps, vizId, id, qVizHandler.qVizLoading])
+
+  useEffect(()=>{
+      if(qVizHandler.qViz){
+        qVizHandler.qViz.show(vizId)
+      }
+      return ()=>{
+        if(qVizHandler.qViz!==null){
+          qApp&&qApp.destroySessionObject(vizId)
+        }
+      }
+    }, [qVizHandler, vizId])
+    return({...qVizHandler, vizId})
+  }
 
 export default useQVizHandler
