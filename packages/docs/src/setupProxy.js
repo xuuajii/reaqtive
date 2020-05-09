2d848882cfb373bf217db30545a354b71d681b41
Paolo Deregibus <paolo.deregibus@gmail.com>
2020-05-09 21:00:20 +0200

    updated http-proxy-mddleware, added to dependencides and updated setup proxy

--- packages/docs/src/setupProxy.js
+++ packages/docs/src/setupProxy.js
@@ -2,18 +2,21 @@
 //Copyright (c) 2019 by Paolo Deregibus. All Rights Reserved.
 //
 
-const proxy = require('http-proxy-middleware');
+//const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const qConfig = require('./q-config');
 //console.log(qConfig)
 const protocol = `http${((qConfig.secure === true) ? 's' : '')}`;
 const host = qConfig.host;
 const port = qConfig.port;
 
+console.log('***************************************************************');
 console.log('Reaqtive will use this URL to connect to Qlik:', protocol+'://'+host+':'+port);
+console.log('***************************************************************');
 
 module.exports = app => {
   app.use(['*/resources/*','*/extension/*','*/extensions/*','*/Exports/*','/api/*','*/sockjs-node/*'],
-  proxy({
+  createProxyMiddleware({
           target: `${protocol}://${host}:${port}`,//'http://localhost:4848',//
           ws: true,
           wss: true,
