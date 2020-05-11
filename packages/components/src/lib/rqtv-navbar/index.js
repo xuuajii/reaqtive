dc281afb38bc0be84285b48981762fafe4c990d1
Paolo Deregibus <paolo.deregibus@gmail.com>
2020-05-11 18:05:50 +0200

    reviewed RqtvNavbar to work fixed-top and non fixed-top

--- packages/components/src/lib/rqtv-navbar/index.js
+++ packages/components/src/lib/rqtv-navbar/index.js
@@ -13,14 +13,18 @@ import {RqtvAppContext} from '../contexts/rqtv-app-context'
 /**
  * RqtvNavbar
  *
- * It is a component that renders the top navbar of the reaqtive app. It is based on bootstrap navbar
- * Styles can be customized via css (or scss)
+ * It is a component that renders the top navbar of the reaqtive app and add spacing to the top of the page if the navbar is fixed top.
+ * It includes the [RqtvCurrentSelections](#rqtvcurrentselections) component and the [RqtvSearchObject](#rqtvsearchobject) component.
+ * It is based on bootstrap navbar, its styles can be customized using navbarClassName prop which will be passed to the navbar itself or via sass/css
  */
 
 const RqtvNavbar = props => {
-  const { searchFieldsMatch, showSideMenuToggle } = props
-  const rqtvApp = useContext(RqtvAppContext)
-
+  const { searchFieldsMatch, showSideMenuToggle, fixedTop, navbarClassName } = props
+  const rqtvApp = useContext(RqtvAppContext)||{}
+  const title = props.title?props.title:rqtvApp.title
+  const brandUrl=props.brandUrl?props.brandUrl:rqtvApp.brandUrl
+  const brandImgUrl=props.brandImgUrl?props.brandImgUrl:rqtvApp.brand
+  const brandImgStyle=props.brandImgStyle?props.brandImgStyle:rqtvApp.brandStyle
   const [showCurrentSelections , setShowCurrentSelections] = useState(props.showCurrentSelections)
 
   const fieldList = searchFieldsMatch?rqtvApp.filterFieldList(rqtvApp.enhancedFieldList, searchFieldsMatch):rqtvApp.searchFieldList
@@ -31,7 +35,7 @@ const RqtvNavbar = props => {
 
   return(
     <>
-      <Navbar className="fixed-top rqtv-navbar">
+      <Navbar className={`${fixedTop===true?'fixed-top':''} rqtv-navbar ${navbarClassName}`}>
         <>
           <div className="navbar-brand-container">
           {showSideMenuToggle&&
@@ -39,8 +43,8 @@ const RqtvNavbar = props => {
               <HamburgerMenu isOpen={props.sideMenuActive}/>
             </Button>
           }
-            <NavbarBrand url={rqtvApp.brandUrl} imgUrl={rqtvApp.brand} imgStyle={rqtvApp.brandStyle}>
-              {rqtvApp.title}
+            <NavbarBrand url={brandUrl} imgUrl={brandImgUrl} imgStyle={brandImgStyle}>
+              {title}
             </NavbarBrand>
           </div>
         </>
@@ -48,8 +52,10 @@ const RqtvNavbar = props => {
           <RqtvCurrentSelections hidden={!showCurrentSelections} hidePrefix={hidePrefix} customLoading={()=><div/>}/>
           {props.showSearch&&
             <RqtvSearchObject
+              fixedTop={props.fixedTop}
+              useBackdrop={props.fixedTop}
               onOpen={()=>{
-                props.closeSideMenu()
+                props.closeSideMenu&&props.closeSideMenu()
                 setShowCurrentSelections(false)
               }}
               onClose={()=>{
@@ -57,8 +63,7 @@ const RqtvNavbar = props => {
               }}
               searchFields={fieldList&&fieldList.map(field=>field.qName)}
               expandFrom="right"
-              useBackdrop={true}
-              resultsHeight={'100%'}
+              resultsHeight={props.fixedTop?'100%':props.searchResultsHeight}
             />
           }
         </NavbarNav>
@@ -69,6 +74,14 @@ const RqtvNavbar = props => {
 
 RqtvNavbar.propTypes={
   /**
+   * if true the navbar would be fix positioned at the top of the page
+   */
+  fixedTop:PropTypes.bool,
+  /**
+   * css classes that will be passed to the navbar div
+   */
+  navbarClassName:PropTypes.string,
+  /**
    * function fired when clicking on the HamburgerMenu button
    */
   onToggleMenu:PropTypes.func,
@@ -77,14 +90,26 @@ RqtvNavbar.propTypes={
    */
   showCurrentSelections:PropTypes.bool,
   /**
+   * show/hide the global search-object
+   */
+  showSearch:PropTypes.bool,
+  /**
    * show/hide hamburger menu
    */
-  showSideMenuToggle:PropTypes.bool
+  showSideMenuToggle:PropTypes.bool,
+  brandUrl:PropTypes.string,
+  brandImgUrl:PropTypes.string,
+  brandImgStyle:PropTypes.object,
+  searchResultsHeight:PropTypes.oneOf(PropTypes.string,PropTypes.number)
 }
 
 RqtvNavbar.defaultProps={
+  fixedTop:true,
+  navbarClassName:'',
   showCurrentSelections:true,
-  showSideMenuToggle:true
+  showSideMenuToggle:true,
+  showSearch:true,
+  searchResultsHeight:500
 }
 
 
