408daa2688487547662bdc2d85ad2fb6cec49407
Paolo Deregibus <paolo.deregibus@gmail.com>
2020-05-18 10:14:16 +0200

    added response interceptors

--- packages/q/dist/contexts/q-global.js
+++ packages/q/dist/contexts/q-global.js
@@ -25,6 +25,28 @@ const schema = require('enigma.js/schemas/12.20.0.json');
 
 const SenseUtilities = require('enigma.js/sense-utilities');
 
+const MAX_RETRIES = 3;
+const responseInterceptors = [{
+  // We only want to handle failed responses from QIX Engine:
+  onRejected: function retryAbortedError(sessionReference, request, error) {
+    // We only want to handle aborted QIX errors:
+    if (error.code === schema.enums.LocalizedErrorCode.LOCERR_GENERIC_ABORTED) {
+      // We keep track of how many consecutive times we have tried to do this call:
+      request.tries = (request.tries || 0) + 1; // We do not want to get stuck in an infinite loop here if something has gone
+      // awry, so we only retry until we have reached MAX_RETRIES:
+
+      if (request.tries <= MAX_RETRIES) {
+        return request.retry();
+      }
+    } // If it was not an aborted QIX call, or if we reached MAX_RETRIES, we let the error
+    // trickle down to potential other interceptors, and finally down to resolving/rejecting
+    // the initial promise that the user got when invoking the QIX method:
+
+
+    return this.Promise.reject(error);
+  }
+}];
+
 const QGlobal = _react.default.createContext();
 /**
  * QGlobal
@@ -56,7 +78,8 @@ const QGlobalProvider = props => {
       const url = SenseUtilities.buildUrl(props.qConfig);
       const session = enigma.create({
         schema,
-        url
+        url,
+        responseInterceptors
       });
       return new Promise(resolve => {
         resolve(session.open());
@@ -81,7 +104,7 @@ const QGlobalProvider = props => {
     value: qGlobalHandler,
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 38
+      lineNumber: 59
     },
     __self: void 0
   }, props.children);
